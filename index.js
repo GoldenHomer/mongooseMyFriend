bf567500254f4eaa77de2e68ddaa9a71f018a513
Jose Contreras <jocontre@joselcontreras.com>
2014-08-15 01:06:03 -0700

    Cleaning up

--- index.js
+++ index.js
@@ -18,8 +18,16 @@ var userSchema = new Schema({
 	name : String,
 	age : Number,
 	DOB : Date,
-	isAlive : Boolean
-});
+	isAlive : Boolean,
+	}, {strict: false}) // allow properties not defined in schema.);
+	userSchema.pre('save', function(next){
+		var err = new Error('Game over man.')
+		next(err); // runs the next middleware in parallel
+	});
+
+userSchema.methods.isYounger = function(){ // A method for use in all User models.
+	return this.model('User').age < 50 ? true : false;
+}
 
 var User = mongoose.model('User', userSchema);
 
@@ -32,4 +40,10 @@ var Bort = new User({
 
 Bort.save(function(err, data){
 	err ? console.log(err) : console.log('Saved : ' + data);
+});
+
+/*myDoc.save(function(err){
+	console.log(err.message);
 })
+*/
+console.log('isYounger : ' + Bort.isYounger());
